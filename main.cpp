7d98488a8f97b6fd8d93f284f95cb18c716331df
pengl <pengl@keyayun.com>
2019-04-22 02:07:03 -0700

    modify for enable UMI extraction

--- main.cpp
+++ main.cpp
@@ -4,80 +4,207 @@
 #include <string>
 #include "kseq.h"
 #include "cmdline.h"
+
 // g++ -std=c++11 main.cpp -lz -o filter
 
 using namespace std;
 KSEQ_INIT(gzFile, gzread)
 
-void write_seq(const kseq_t *seq, gzFile file) {
-  gzputc(file, '@');
-  gzputs(file, seq->name.s);
-  if (seq->comment.s) {
-    gzputc(file, ' ');
-    gzputs(file, seq->comment.s);
-  }
-  gzputc(file, '\n');
-  gzputs(file, seq->seq.s);
-  gzputs(file, "\n+\n");
-  gzputs(file, seq->qual.s);
-  gzputc(file, '\n');
-}
-
 cmdline::parser parameter(int argc, char *argv[]) {
-  cmdline::parser opt;
-  opt.add<string>("read1", '1', "input read1, compressed or not, must be Pherd33", true, "");
-  opt.add<string>("read2", '2', "input read2, compressed or not", true, "");
-  opt.add<string>("out1", '3', "out read1, compressed", true, "");
-  opt.add<string>("out2", '4', "out read2, compressed", true, "");
-  opt.add<int>("qual", 'q', "mean reads quality cut off, default is 30", false, 30);
-  opt.parse_check(argc, argv);
-  return opt;
+    cmdline::parser opt;
+    opt.add<string>("read1", '1', "input read1, compressed or uncompressed, must be Pherd33", true, "");
+    opt.add<string>("read2", '2', "input read2", true, "");
+    opt.add<string>("out1", '3', "out read1, compressed", true, "");
+    opt.add<string>("out2", '4', "out read2, compressed", true, "");
+
+    opt.add<int>("qual", 'q', "mean read quality cut off, default is 30", false, 30);
+    opt.add("umi", 'u', "extract UMI sequence, default false");
+
+    // 10U8B3F*R
+    opt.add<int>("umiStart", '\0', "start position of UMI sequence, 0 based", false);
+    opt.add<int>("umiLength", '\0', "umi length", false);
+    opt.add<int>("readStart", '\0', "start position of real sequence, 0 based", false);
+    opt.add<int>("readLength", '\0', "real sequence length, it can be ignored when the sequence reach the end", false);
+
+    opt.parse_check(argc, argv);
+    return opt;
 }
 
-int average_quality(const kseq_t *seq) {
-  char *quality = seq->qual.s;
-  int sumQ = 0;
-  for (; *quality; quality++)
-    sumQ += *quality - 33;
-  return sumQ / seq->seq.l;
+//
+int average_quality(const kseq_t *read) {
+   char *quality = read->qual.s;
+   int sumQ = 0;
+   for (; *quality; quality++)
+       sumQ += *quality - 33; // Pherd33
+   return sumQ / read->seq.l;
+}
+int average_quality(const kseq_t* read, int start) {
+    string quality = read->qual.s;
+    quality = quality.substr(start); //
+    int sumQ = 0, L = quality.length();
+    for(int i = 0; i < L; i++)
+        sumQ += quality[i] - 33;
+    return sumQ / L;
+}
+int average_quality(const kseq_t* read, int start, int length) {
+    string quality = read->qual.s;
+    quality = quality.substr(start, length); //
+    int sumQ = 0, L = quality.length();
+    for(int i = 0; i < L; i++)
+        sumQ += quality[i] - 33;
+    return sumQ / L;
 }
 
-bool pair_check(const kseq_t *seq1, const kseq_t *seq2, int Q) {
-  int q1 = average_quality(seq1);
-  int q2 = average_quality(seq2);
-  if (q1 >= Q and q2 >= Q)
-    return true;
-  else
-    return false;
+//
+string get_umi(const kseq_t *read1, const kseq_t *read2, int start, int length, char connect = ':') {
+    string umi, sequence1, sequence2;
+    sequence1 = read1->seq.s;
+    sequence2 = read2->seq.s;
+    umi = sequence1.substr(start, length) + connect + sequence1.substr(start, length);
+    return umi;
 }
 
+// 
+void write_read(const kseq_t *read, gzFile file) {
+    string lines = "@";
+    lines.append(read->name.s);
+    if(read->comment.l) { // comment.s
+        lines.append(" ");
+        lines.append(read->comment.s);
+    }
+    lines.append("\n");
+    lines.append(read->seq.s);
+    lines.append("\n+\n");
+    lines.append(read->qual.s);
+    lines.append("\n");
+    gzputs(file, lines.c_str()); // IO
+}
+void write_read(const kseq_t *read, gzFile file, string umi, int start) {
+    string lines = "@";
+    lines.append(read->name.s);
+    lines.append(":" + umi); // umi
+    if(read->comment.l) {
+        lines.append(" ");
+        lines.append(read->comment.s);
+    }
+    lines.append("\n");
+    string sequence = read->seq.s;
+    lines.append(sequence.substr(start)); //
+    lines.append("\n+\n");
+    string quality = read->qual.s;
+    lines.append(quality.substr(start)); //
+    lines.append("\n");
+    gzputs(file, lines.c_str());
+}
+void write_read(const kseq_t *read, gzFile file, string umi, int start, int length) {
+    string lines = "@";
+    lines.append(read->name.s);
+    lines.append(":" + umi);
+    if(read->comment.l) {
+        lines.append(" ");
+        lines.append(read->comment.s);
+    }
+    lines.append("\n");
+    string sequence = read->seq.s;
+    lines.append(sequence.substr(start, length)); //
+    lines.append("\n+\n");
+    string quality = read->qual.s;
+    lines.append(quality.substr(start, length)); //
+    lines.append("\n");
+    gzputs(file, lines.c_str()); //
+}
+
+//
+int filter(kseq_t *reads1, kseq_t *reads2, gzFile out1, gzFile out2, int cutQ) {
+    int q1 = average_quality(reads1);
+    if(q1 < cutQ)
+        return 1;
+    int q2 = average_quality(reads2);
+    if(q2 < cutQ)
+        return 2;
+
+    write_read(reads1, out1);
+    write_read(reads2, out2);
+    return 0;
+}
+int filter(kseq_t *reads1, kseq_t *reads2, gzFile out1, gzFile out2, int cutQ, int umi_start, int umi_length, int seq_start) {
+    int q1 = average_quality(reads1, seq_start);
+    if(q1 < cutQ)
+        return 1;
+    int q2 = average_quality(reads2, seq_start);
+    if(q2 < cutQ)
+        return 2;
+    string umi = get_umi(reads1, reads2, umi_start, umi_length, ':');
+    write_read(reads1, out1, umi, seq_start);
+    write_read(reads2, out2, umi, seq_start);
+    return 0;
+}
+int filter(kseq_t *reads1, kseq_t *reads2, gzFile out1, gzFile out2, int cutQ, int umi_start, int umi_length, int seq_start, int seq_length) {
+    int q1 = average_quality(reads1, seq_start, seq_length);
+    if(q1 < cutQ)
+        return 1;
+    int q2 = average_quality(reads2, seq_start, seq_length);
+    if(q2 < cutQ)
+        return 2;
+    string umi = get_umi(reads1, reads2, umi_start, umi_length, ':');
+    write_read(reads1, out1, umi, seq_start, seq_length);
+    write_read(reads2, out2, umi, seq_start, seq_length);
+}
+
+//
 int main(int argc, char *argv[]) {
-  cmdline::parser opt = parameter(argc, argv);
-  gzFile fp1, fp2, out1, out2;
-  // open the file handler
-  fp1 = gzopen(opt.get<string>("read1").c_str(), "r");
-  fp2 = gzopen(opt.get<string>("read2").c_str(), "r");
-  out1 = gzopen(opt.get<string>("out1").c_str(), "wb");
-  out2 = gzopen(opt.get<string>("out2").c_str(), "wb");
-  int qual = opt.get<int>("qual");
-  // initialize seq
-  kseq_t *seq1 = kseq_init(fp1);
-  kseq_t *seq2 = kseq_init(fp2);
-  // read sequence
-  while (kseq_read(seq1) >= 0) {
-      kseq_read(seq2);
-      bool qc = pair_check(seq1, seq2, qual);
-      if (qc) {
-         write_seq(seq1, out1);
-         write_seq(seq2, out2);
-      }
-  }
-  // destroy seq
-  kseq_destroy(seq1);
-  kseq_destroy(seq2);
-  // close the file handler
-  gzclose(fp1); gzclose(fp2);
-  gzclose(out1); gzclose(out2);
+    // parameter
+    cmdline::parser opt = parameter(argc, argv);
+    int cutQ = opt.get<int>("qual");
+    bool treat_umi = opt.exist("umi");
+    int umi_start = -1, umi_length = -1, seq_start = -1, seq_length = 0; //#
+    if(treat_umi) {
+        if(opt.exist("umiStart") && opt.exist("umiLength") && opt.exist("readStart")) {
+            umi_start = opt.get<int>("umiStart");
+            umi_length = opt.get<int>("umiLength");
+            seq_start = opt.get<int>("readStart");
+        } else {
+            cerr << "Error: --umiStart, --umiLength, --readStart, these 3 are all need for umi treat" << endl;
+            return 3; // exit
+        }
+        if(opt.exist("readLength"))
+            seq_length = opt.get<int>("readLength");
+    } else {
+        if(opt.exist("umiStart") || opt.exist("umiLength") || opt.exist("readStart")) {
+            cerr << "Error: it seems that you want do umi treat, but lack of --umi" << endl;
+            return 5;
+        }
+    }
+  
+    // file handler
+    gzFile fp1, fp2, out1, out2;
+    fp1 = gzopen(opt.get<string>("read1").c_str(), "r");
+    fp2 = gzopen(opt.get<string>("read2").c_str(), "r");
+    out1 = gzopen(opt.get<string>("out1").c_str(), "wb");
+    out2 = gzopen(opt.get<string>("out2").c_str(), "wb");
+  
+    // initialize read
+    kseq_t *reads1 = kseq_init(fp1);
+    kseq_t *reads2 = kseq_init(fp2);
 
-  return 0;
+    while (kseq_read(reads1) >= 0) {
+        kseq_read(reads2);
+        if(treat_umi) {
+            if(seq_length) // not 0
+                filter(reads1, reads2, out1, out2, cutQ, umi_start, umi_length, seq_start, seq_length);
+            else
+                filter(reads1, reads2, out1, out2, cutQ, umi_start, umi_length, seq_start);
+        } else
+            filter(reads1, reads2, out1, out2, cutQ);
+    }
+
+    // destroy read
+    kseq_destroy(reads1);
+    kseq_destroy(reads2);
+  
+    // close file
+    gzclose(fp1); gzclose(fp2);
+    gzclose(out1); gzclose(out2);
+  
+    return 0;
 }
