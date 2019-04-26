54a5c8d0cba2716779dd811a0738248c33490b12
pengl <pengl@keyayun.com>
2019-04-26 01:50:26 -0700

    add more parameters

--- main.cpp
+++ main.cpp
@@ -4,24 +4,27 @@
 #include <string>
 #include "kseq.h"
 #include "cmdline.h"
-// g++ -std=c++11 main.cpp -lz -o filter
+// g++ -std=c++11 main.cpp -lz
 
 using namespace std;
 KSEQ_INIT(gzFile, gzread)
 
 cmdline::parser parameter(int argc, char *argv[]) {
     cmdline::parser opt;
-    opt.add<string>("read1", '1', "input read1, compressed or uncompressed, must be Pherd33", true, "");
-    opt.add<string>("read2", '2', "input read2", true, "");
-    opt.add<string>("out1", '3', "out read1, compressed", true, "");
-    opt.add<string>("out2", '4', "out read2, compressed", true, "");
-    opt.add<int>("qual", 'q', "mean read quality cut off, default is 30", false, 30);
-    opt.add("umi", 'u', "extract UMI sequence, default false");
-    // 10U8B3F*R
-    opt.add<int>("umiStart", '\0', "start position of UMI sequence, 0 based", false);
-    opt.add<int>("umiLength", '\0', "umi length", false);
-    opt.add<int>("readStart", '\0', "start position of real sequence, 0 based", false);
+    opt.add<string>("read1", '1', "Required, input read1, it can be compressed or not.", true);
+    opt.add<string>("read2", '2', "Required, input read2.", true);
+    opt.add<string>("out1", '3', "Required, out read1, compressed.", true);
+    opt.add<string>("out2", '4', "Required, out read2.", true);
+    opt.add<int>("qual", 'q', "mean quality threshold for discard reads, default is 30.", false, 30);
+    opt.add("umi", '\0', "extract UMI sequence, default is NO.");
+    opt.add<int>("umiStart", '\0', "start position(0 based) of UMI sequence, required for UMI.", false);
+    opt.add<int>("umiLength", '\0', "UMI sequence length at one single read, required for UMI.", false);
+    opt.add<string>("connection", '\0', "the character between readsID and UMI, it can be space(S), colon(C), or underline(U), default is space",
+                 false, "C", cmdline::oneof<string>("S", "C", "U"));
+    opt.add("disComment", '\0', "for disable reads's comment, default is NO.");
+    opt.add<int>("readStart", '\0', "start position of real sequence, required for UMI.", false);
     opt.add<int>("readLength", '\0', "real sequence length, it can be ignored when the sequence reach the end", false);
+    opt.add<int>("level", '\0', "compression level for gzip output (1 ~ 9). 1 is fastest, 9 is smallest, default is 4.", false, 4);
     opt.parse_check(argc, argv);
     return opt;
 }
@@ -36,7 +39,7 @@ int average_quality(const kseq_t* read, int start, int length) {
 }
 
 string get_umi(const kseq_t *read1, const kseq_t *read2,
-               int start, int length, char connect = ':') {
+               int start, int length, char connect = '_') {
     string umi, sequence1, sequence2;
     sequence1 = read1->seq.s;
     sequence2 = read2->seq.s;
@@ -47,15 +50,16 @@ string get_umi(const kseq_t *read1, const kseq_t *read2,
 }
 
 void write_read(const kseq_t *read, gzFile file,
+                bool add_comment, int level, char p,
                 string umi, int start, int length) {
     // inspired by chen
-    gzsetparams(file, 4, Z_DEFAULT_STRATEGY); // TODO set for level
+    gzsetparams(file, level, Z_DEFAULT_STRATEGY);
     gzbuffer(file, 1024*1024);
     gzputc(file, '@'); gzputs(file, read->name.s);
     if(umi.length()) {
-        gzputc(file, ' '); gzputs(file, umi.c_str());
+        gzputc(file, p); gzputs(file, umi.c_str());
     }
-    if(read->comment.l) {
+    if(add_comment && read->comment.l) {
         gzputc(file, ' '); gzputs(file, read->comment.s);
     }
     gzputc(file, '\n');
@@ -69,25 +73,27 @@ int main(int argc, char *argv[]) {
     // parameter logic check
     cmdline::parser opt = parameter(argc, argv);
     int cutQ = opt.get<int>("qual");
+    int level = opt.get<int>("level");
+    bool add_comment = !opt.exist("disComment");
     bool treat_umi = opt.exist("umi");
-    int umi_start = 0, umi_length = 0;
-    int seq_start = 0, seq_length = 0;
+    int umi_start = treat_umi && opt.exist("umiStart") ? opt.get<int>("umiStart"): 0;
+    int umi_length = treat_umi && opt.exist("umiLength")? opt.get<int>("umiLength"): 0;
+    string connection = opt.get<string>("connection");
+    char prefix;
+    if(connection == "S") prefix = ' ';
+    if(connection == "C") prefix = ':';
+    if(connection == "U") prefix = '_';
+    int seq_start = treat_umi && opt.exist("readStart")? opt.get<int>("readStart"): 0; //
+    int seq_length = opt.exist("readLength")? opt.get<int>("readLength"): 0; //
     if(treat_umi) {
-        if(opt.exist("umiStart") && opt.exist("umiLength") && opt.exist("readStart")) {
-            umi_start = opt.get<int>("umiStart");
-            umi_length = opt.get<int>("umiLength");
-            seq_start = opt.get<int>("readStart");
-        } else {
-            cerr << "Error: --umiStart, --umiLength, --readStart, these 3 are all need for umi treat" << endl;
-            return 3;
+        if(!opt.exist("umiStart") || !opt.exist("umiLength") || !opt.exist("readStart")) {
+            cerr << "Error: --umiStart, --umiLength and --readStart are all need for umi treat" << endl;
+            return -1;
         }
-
-        if(opt.exist("readLength"))
-            seq_length = opt.get<int>("readLength");
     } else {
         if(opt.exist("umiStart") || opt.exist("umiLength") || opt.exist("readStart")) {
             cerr << "Error: it seems that you want do umi treat, but lack of --umi" << endl;
-            return 4;
+            return -1;
         }
     }
     // file handler
@@ -111,10 +117,10 @@ int main(int argc, char *argv[]) {
         q1 = average_quality(reads1, seq_start, seq1_end);
         q2 = average_quality(reads2, seq_start, seq2_end);
         if(q1 >= cutQ && q2 >= cutQ) {
-            UMISeq = treat_umi? get_umi(reads1, reads2, umi_start, umi_length, ':'): "";
+            UMISeq = treat_umi? get_umi(reads1, reads2, umi_start, umi_length): "";
             // todo multi-thread
-            write_read(reads1, out1, UMISeq, seq_start, seq1_end);
-            write_read(reads2, out2, UMISeq, seq_start, seq2_end);
+            write_read(reads1, out1, add_comment, level, prefix, UMISeq, seq_start, seq1_end);
+            write_read(reads2, out2, add_comment, level, prefix, UMISeq, seq_start, seq2_end);
         }
     }
     // destroy read
